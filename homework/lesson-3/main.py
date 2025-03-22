f74a5452f48e5788ed5628fc126a8eae1d17f5b8
Lozova1988 <lozovayaelena.v@gmail.com>
2025-03-22 12:23:51 +0000

    classwork

---
+++ homework/lesson-3/main.py
@@ -0,0 +1,29 @@
+score = 45
+if score>90:
+    print("Your score is Grade A")
+elif score>80 and score<=90:
+    print("Your score is Grade B")
+elif score>=90 and score<=80:
+    print("Your score is Grade C")
+else:
+    print("Your score is Grade D")
+
+count1=0
+while count1<=100:
+    print(count1)
+    count1=count1+1
+
+
+for count2 in range(0,100):
+    if count2 %2 !=0:
+        print(count2)
+
+
+name=input("What is your name?")
+print("Hi", name)
+
+your_age=input("What is your age?")
+your_weith=input("What is your weith")
+print("Your age is ",your_age, ". Your weith is ", your_weith )
+
+

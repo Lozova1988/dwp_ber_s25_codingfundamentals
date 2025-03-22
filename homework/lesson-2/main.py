d328cbd4ac8716c29fe42064b6a4da3d148d2167
Lozova1988 <lozovayaelena.v@gmail.com>
2025-03-22 09:44:31 +0000

    homework1

---
+++ homework/lesson-2/main.py
@@ -0,0 +1,67 @@
+my_number = 10
+my_string = "Hello, Python!"
+my_float = 3.14
+print("mynumber:", my_number)
+print("myrmystring:", my_string)
+print("myfloat:", my_float)
+
+
+first_name = "Olena"
+last_name = "Lozova"
+full_name = first_name + "    " + last_name
+print("My_full_ name_is" , full_name)
+
+a=5
+b=3
+add_result=a+b
+sub_result=a-b
+mult_result=a*b
+div_result=a/b
+print("Addition:", add_result)
+print("Subtraction:", sub_result)
+print("Multiplication:", mult_result)
+print("Division:", div_result)
+
+
+
+is_greater = 5 > 3
+is_equal = 5 == 3
+is_smaller = 5 < 3
+print("Is greater:", is_greater)
+print("Is equal:", is_equal)
+print("Is smaller:", is_smaller)
+
+bool1 = True
+bool2 = False
+and_result = bool1 and bool2
+or_result = bool1 or bool2
+print("And result:", and_result)
+print("Or result:", or_result)
+
+
+pi = 3.14
+pi_pi = '3.14'
+pi_pi_pi = "3.14"
+print("Are pi and pi_pi equal?", pi == pi_pi, "because pi is a number and pi_pi is a string")
+print("Are pi_pi and pi_pi_pi equal?", pi_pi == pi_pi_pi, "because both pi_pi and pi_pi_pi are the same strings")
+
+
+print(type(pi))
+print(type(pi_pi))
+print(type(pi_pi_pi))
+
+
+
+my_str = "123"
+my_int = int(my_str)
+my_float_converted = float(my_int)
+print("my_str:", my_str)
+print("my_int:", my_int)
+print("my_float_converted:", my_float_converted)
+
+
+celsius = 25
+fahrenheit = (celsius * 9/5) + 32
+print("Original temperature in Celsius:", celsius)
+print("Converted temperature in Fahrenheit:", fahrenheit)
+print("heeuu")

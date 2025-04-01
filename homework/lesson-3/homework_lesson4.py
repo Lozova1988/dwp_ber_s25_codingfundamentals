56bcf2cea67bc29f873db1c6e4fa393a9b28993f
Lozova1988 <lozovayaelena.v@gmail.com>
2025-04-01 20:52:43 +0000

    homework4

---
+++ homework/lesson-3/homework_lesson4.py
@@ -0,0 +1,14 @@
+scores = [5, 6, 6, 13, 6, 16, 2, 4, 6, 15, 3, 7, 20, 3, 24, 24, 1, 23, 3, 3, 3, 21, 7, 2, 12]
+print("The length of scores is:  ", len(scores))
+
+count_three=scores.count(3)
+print("Count of number 3 is: ", count_three)
+
+print("Maximum in scores is:  ", max(scores))
+
+list_1 = ["foo", 2, "bar", 3, "baz", "spam", 4]
+list_2 = ["1", 2, "3", 3, "4", "foo", "pasm", "bar"]
+comm_elements=set(list_1)&set(list_2)
+print("The common elements are:  ",comm_elements)
+
+all_numbers = [111, 32, -9, -45, -17, 9, 85, -10]

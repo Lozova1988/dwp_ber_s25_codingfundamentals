438ae124a2548a11f80ebf04e3eb4adf8c8d0d10
Lozova1988 <lozovayaelena.v@gmail.com>
2025-05-01 14:12:01 +0000

    homework_lesson7

---
+++ homework/lesson-7/homework_lesson_7.py
@@ -0,0 +1,17 @@
+items_list = []
+while True:
+    item=(input("Enter items for shopping list (Tipe `done` to finish): ").strip( ))
+    if item == "":
+        continue
+    if item.lower() =="done":
+        break
+    items_list.append(item)
+items_list.sort()   
+print(f"Your shopping list is: ", items_list)
+
+counter = 1
+for item in items_list:
+    print(str(counter)+". "+ item)
+    counter=counter+1
+
+print("Total number of items: " + str(len(items_list)))
